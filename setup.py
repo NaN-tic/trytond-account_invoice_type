61bcbf875c92e13b4d05754ff94825fe7a74ec77
Àngel Àlvarez <angel@nan-tic.com>
2020-04-24 05:02:28 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-# This file is part account_invoice_type module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# encoding: utf-8
+
+from setuptools import setup
+import re#!/usr/bin/env python
+# encoding: utf-8
 
 from setuptools import setup
 import re
@@ -9,6 +11,8 @@ import os
 import io
 from configparser import ConfigParser
 
+MODULE = 'account_invoice_type'
+PREFIX = 'trytonspain'
 MODULE2PREFIX = {}
 
 
@@ -33,44 +37,53 @@ info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
+
 version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonspain_account_invoice_type'
-download_url = 'https://bitbucket.org/trytonspain/trytond-account_invoice_type'
 
 requires = []
+
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         prefix = MODULE2PREFIX.get(dep, 'trytond')
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
+requires += [get_require_version('trytond_account_invoice')]
 
-tests_require = []
+tests_require = [
+    get_require_version('proteus'),
+    
+    ]
+
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'master'
+else:
+    branch = series
+
 dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='Tryton Tryton Account Invoice Type Module',
+    description='',
     long_description=read('README'),
-    author='TrytonSpain',
-    author_email='info@trytonspain.com',
-    url='https://bitbucket.org/trytonspain/',
-    download_url=download_url,
-    keywords='',
-    package_dir={'trytond.modules.account_invoice_type': '.'},
+    author='trytonspain',
+    url='http://www.nan-tic.com/',
+    download_url='https://github.com:trytonspain/trytond-account_invoice_type',
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_invoice_type',
-        'trytond.modules.account_invoice_type.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.account_invoice_type': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -84,12 +97,8 @@ setup(name=name,
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
@@ -98,10 +107,10 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_invoice_type = trytond.modules.account_invoice_type
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
+
     )
